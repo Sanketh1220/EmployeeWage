101361ba3bf3ad256063c591c9b0a0d85db17f9b
Sanketh1220 <sanketh.babbur11@gmail.com>
2021-03-13 15:46:59 +0530

    added code for assuming max working days or hours

--- empWage.sh
+++ empWage.sh
@@ -4,27 +4,32 @@
 IS_PRESENT_PART_TIME=2
 IS_PRESENT_FULL_TIME=1
 EMP_RATE_PER_HR=20
-TOTAL_WORKING_DAYS=20
+MAX_WORKING_DAYS=20
+MAX_WORKING_HOURS=90
 
-# Variable
-
-for (( counter=1; counter <= $TOTAL_WORKING_DAYS; counter++ ))
+totalWorkingHours=0
+totalWorkingDays=0
+	
+while [ $totalWorkingDays -lt $MAX_WORKING_DAYS ] && [ $totalWorkingHours -lt $MAX_WORKING_HOURS ]
 do
-
-empCheck=$(( RANDOM % 3 ))
-
-	case $empCheck in
-		$IS_PRESENT_FULL_TIME)
+	# variable
+	empCheck=$(( RANDOM % 3 ))
+	(( totalWorkingDays++ ))
+	
+		case $empCheck in
+			$IS_PRESENT_FULL_TIME)
 			 empHrs=8 ;; 
 
-		$IS_PRESENT_PART_TIME)
+			$IS_PRESENT_PART_TIME)
 			 empHrs=4 ;;
 		
-		*)
+			*)
 			 empHrs=0 ;;
 
-esac
-
-	salary=$(( $empHrs * $EMP_RATE_PER_HR ))
+	esac
+		
+	totalWorkingHours=$(( $totalWorkingHours + $empHrs ))
 
 done
+
+salary=$(( $totalWorkingHours * $EMP_RATE_PER_HR ))
